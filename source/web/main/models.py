4b07812132c1a488a946b4441c63f7cf60f334d6
FeshDr <maksimchikov03@mail.ru>
2024-11-21 17:43:01 +0300

    TS-59: added __str__ method to models

--- source/web/main/models.py
+++ source/web/main/models.py
@@ -31,10 +31,19 @@ class Profile(BaseModel):
     rating = models.IntegerField(default=0)
     achievments = models.ManyToManyField('Achievment', through='UserAchievments')
 
+    def __str__(self):
+        return f"#{self.id}/{self.last_name} {self.first_name[0] if self.first_name != '' else self.first_name}. {self.middle_name[0] if self.middle_name != '' else self.middle_name}. ({self.user.username})"
+    
+    @property
+    def fullname(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
 
 class Achievment(BaseModel):
     name = models.CharField(max_length=60)
     creator = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"#{self.id} {self.name}"
 
 
 class UserAchievments(BaseModel):
@@ -45,6 +54,9 @@ class TaskType(BaseModel):
     name = models.CharField(max_length=40)
     tester_name = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(BaseModel):
     name = models.CharField(max_length=100)
@@ -54,48 +66,76 @@ class Task(BaseModel):
     output = models.CharField(max_length=400, null=True, blank=True)
     note = models.CharField(max_length=400, null=True, blank=True)
 
+    def __str__(self):
+        return f"#{self.id}/{self.name} ({self.task_type})" 
+
 
 class Test(BaseModel):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     test_input = models.TextField(null=True, blank=True)
     test_output = models.TextField()
+    order = models.IntegerField()
+
+    def __str__(self):
+        return f"Test #{self.order} for {self.task}"
 
 
 class Verdict(BaseModel):
     name = models.CharField(max_length=100)
     short_name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"#{self.id} {self.name}"
+
 
 class AnswerOption(BaseModel):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"Answer Option #{self.id} for {self.task}"
 
 
 class Compiler(BaseModel):
     name = models.CharField(max_length=30)
     extension = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"#{self.id} {self.name} ({self.extension})"
+
 
 class AnswerCode(BaseModel):
     code = models.CharField(max_length=30)
     compiler = models.ForeignKey(Compiler, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.code} ({self.compiler})"
+
 
 class Answer(BaseModel):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    answer_option = models.ForeignKey(AnswerOption,null=True, blank=True, on_delete=models.SET_NULL)
-    answer_code = models.ForeignKey(AnswerCode,null=True, blank=True, on_delete=models.SET_NULL)
-    verdict = models.ForeignKey(Verdict,null=True, on_delete=models.SET_NULL)
+    answer_option = models.ForeignKey(AnswerOption, null=True, blank=True, on_delete=models.SET_NULL)
+    answer_code = models.ForeignKey(AnswerCode, null=True, blank=True, on_delete=models.SET_NULL)
+    verdict = models.ForeignKey(Verdict, null=True, on_delete=models.SET_NULL)
     penalty = models.IntegerField()
+
+    def __str__(self):
+        return f"Answer #{self.id} for {self.task}"
 
 
 class Category(BaseModel):
     category_name = models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.category_name
+
 
 class ContestType(BaseModel):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Contest(BaseModel):
@@ -108,6 +148,9 @@ class Contest(BaseModel):
     compilers = models.ManyToManyField(Compiler, through='CompilerOncontest')
     users = models.ManyToManyField(Profile, through='UserToContest', related_name='contests')
     type = models.ForeignKey(ContestType, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"#{self.id} {self.name}"
 
 
 class TaskOnContest(BaseModel):
@@ -128,6 +171,9 @@ class CompilerOnContest(BaseModel):
 
 class ContestRole(BaseModel):
     name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
 
 
 class UserToContest(BaseModel):
