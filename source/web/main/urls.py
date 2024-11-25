785170da1a283bf1643100ab78aeb1a6faad589b
Vladimir Velikovich <vvelikovich@gmail.com>
2024-11-26 00:11:35 +0300

    TS-46: Added opened contests on Index Page

--- source/web/main/urls.py
+++ source/web/main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from main.views import SignUpView, index, UserLoginView
+from main.views import (
+    IndexView,
+    SignUpView,
+    UserLoginView,
+)
 
 
 urlpatterns = [
-    path("", index, name='index'),
+    path("", IndexView.as_view(), name='index'),
     path("users/logout/", LogoutView.as_view(), name="logout"),
     path("users/signup/", SignUpView.as_view(), name="signup"),
     path("users/login/", UserLoginView.as_view(), name="login"),
